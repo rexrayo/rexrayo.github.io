f4a8beed22681ffbe4351e4588006d21b4862afa
kaller <rexrayo15@gmail.com>
2020-09-14 15:50:51 -0430

    Change app

--- sw.js
+++ sw.js
@@ -102,12 +102,10 @@ self.addEventListener('install', evt => {
     })
   );
 });
-
 // activate event
 self.addEventListener('activate', evt => {
   //console.log('service worker activated');
 });
-
 // fetch event
 self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
