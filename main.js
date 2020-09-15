ec49a80653043f40e7479805d627785eb8663b0b
kaller <rexrayo15@gmail.com>
2020-09-14 19:48:24 -0430

    Cambios

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ function outputUsers(users) {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw.js')
+      .register('/sw.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
